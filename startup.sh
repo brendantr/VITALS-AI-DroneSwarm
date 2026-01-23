d769a6b6931e741ba6aa1057e79cc9b0318fc8c1
Alex Gershfeld <alex.gershfeld@gmail.com>
2026-01-22 19:17:54 -0500

    Alex [Add]: the rest of the startup

--- startup.sh
+++ startup.sh
@@ -1,5 +1,30 @@
-./QGroundControl-x86_64.AppImage
+sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
+sudo apt install libfuse2 -y
+sudo apt install libxcb-xinerama0 libxkbcommon-x11-0 libxcb-cursor-dev -y
 
+chmod +x ./QGroundControl-x86_64.AppImage
+
+if [! -d "PX4-Autopilot/.git"]; then
+    git clone --recursive https://github.com/PX4/PX4-Autopilot.git
+    bash PX4-Autopilot/Tools/setup/ubuntu.sh
+else
+    echo("PX4-Autopilot already exists")
+fi
+  source ~/.bashrc
+
+./QGroundControl-x86_64.AppImage &
 wait
 
-echo("qgc loaded")
+echo("QGC loaded")
+
+make px4_sitl gz_x500 &
+wait
+
+echo("PX4 SITL started")
+
+cd app
+git pull
+docker compose pull
+docker compose up -d
+
+
