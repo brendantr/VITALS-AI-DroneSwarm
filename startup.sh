0ed978d3e721c79b487da4857a508a7e0c45b5c2
Alex Gershfeld <alex.gershfeld@gmail.com>
2026-01-27 21:22:14 -0500

    Alex [Add]: add apt update

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
+sudo apt update
 sudo apt install gstreamer1.0-plugins-bad gstreamer1.0-libav gstreamer1.0-gl -y
 sudo apt install libfuse2 -y
 sudo apt install libxcb-xinerama0 libxkbcommon-x11-0 libxcb-cursor-dev -y
@@ -19,7 +20,7 @@ sleep 75
 echo "QGC loaded"
 
 cd PX4-Autopilot
-make px4_sitl gz_x500
+make px4_sitl gz_x500 
 
 echo "PX4 SITL started"
 
